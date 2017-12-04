7d3f5e4333b3a96ac50e6eefa107027e8edcaceb
Phanes <punches.chris@gmail.com>
2017-12-04 00:46:34 -0500

    updated license notices

--- src/loaders/helpers.h
+++ src/loaders/helpers.h
@@ -1,6 +1,22 @@
-//
-// Created by phanes on 4/22/17.
-//
+/*
+    Examplar - An automation and testing framework.
+
+    © SURRO INDUSTRIES and Chris Punches, 2017.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
 
 #ifndef FTESTS_HELPERS_H
 #define FTESTS_HELPERS_H
