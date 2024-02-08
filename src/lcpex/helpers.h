bb324087a8b10f9cc0cb2132ae2b0527363ad062
Christopher M. Punches <chris.punches@silogroup.org>
2024-02-08 10:45:26 +0000

    Update src/lcpex/helpers.h

--- src/lcpex/helpers.h
+++ src/lcpex/helpers.h
@@ -16,6 +16,18 @@ enum CHILD_PIPE_NAMES {
 
 #define BUFFER_SIZE 1024
 
-ssize_t write_all(int fd, const void *buf, size_t count);
+ssize_t write_all(int fd, const void *buf, size_t count) {
+    const char *p = (const char *)buf;
+    while (count > 0) {
+        ssize_t written = write(fd, p, count);
+        if (written == -1) {
+            if (errno == EINTR || errno == EAGAIN) continue; // Retry
+            return -1; // Other errors
+        }
+        count -= written;
+        p += written;
+    }
+    return 0;
+}
 
 #endif //LCPEX_HELPERS_H
