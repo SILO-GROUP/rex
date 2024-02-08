cddc271ac73342119b19230121f97df06d9c64df
Christopher M. Punches <chris.punches@silogroup.org>
2024-02-08 10:43:46 +0000

    Update src/lcpex/liblcpex.cpp

--- src/lcpex/liblcpex.cpp
+++ src/lcpex/liblcpex.cpp
@@ -1,19 +1,5 @@
 #include "liblcpex.h"
 
-ssize_t write_all(int fd, const void *buf, size_t count) {
-    const char *p = (const char *)buf;
-    while (count > 0) {
-        ssize_t written = write(fd, p, count);
-        if (written == -1) {
-            if (errno == EINTR || errno == EAGAIN) continue; // Retry
-            return -1; // Other errors
-        }
-        count -= written;
-        p += written;
-    }
-    return 0;
-}
-
 
 std::string prefix_generator(
         std::string command,
